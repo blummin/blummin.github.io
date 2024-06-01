2e0d5ef21be64f03d1ec3d2fbcee06a7f61f9d86
Callum <58528912+callum11t@users.noreply.github.com>
2024-06-01 23:24:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ for (let tim = 0; tim < 4; tim++){
   const random = Math.floor(Math.random() * arr.length + 1);
   splashText.innerHTML = arr[Math.floor(Math.random()*arr.length)] + "!";
   splashText.animate([
-    { transform: 'translateY(2vw)' },
+    { transform: 'translateY(-2vw)' },
     { transform: 'translateY(0vw)' }
   ], {
-    duration: 500,
+    duration: 300,
     iterations: 5,
     easing: "ease-out",
   });
