b5ab29c1f0c64019024c84af5f3b87cb69d00f91
blummin <bluetanimates+2@gmail.com>
2024-06-01 21:37:11 +0100

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,10 +1,8 @@
-var html = '<div class="navlink"><a href="index.html">Home</a></div>';
+var html = '<a href="index.html"><button>Home</button></a>';
 
 var section1 = ['Socials']//write one-word link names in this list
 for (i = 0; i<section1.length; i++){
-	html += '<div class="navlink"><a href="' + section1[i].toLowerCase(); + '.html">' + section1[i] + '</a></div>'
+	html += '<div class="navlink"><a href="' + section1[i].toLowerCase(); + '.html"><button>' + section1[i] + '</button></a></div>'
 }
-
-html += '<div class="navlink"><a href="https://youtu.be/dQw4w9WgXcQ">Custom Link</a></div>'
 
 document.getElementById("templaterr-topbar").innerHTML = html;
