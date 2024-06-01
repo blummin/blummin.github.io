870432460b9f45243a5a54351071c05dd9287caa
Callum <58528912+callum11t@users.noreply.github.com>
2024-06-01 21:20:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const splashText = document.getElementById("splashText");
 const random = Math.floor(Math.random() * 9);
 
-fetch('/words.txt')
-  .then(response => response.text())
-  .then((data) => {
-    word = data.readline(random);
-    print(word)
-  })
+var arr = [
+  "amazing",
+  "weird",
+  "hot",
+  "wonderful",
+  "hilarious",
+  "beautiful",
+  "crazy",
+  "cold"
+];
+
+splashText.innerHTML = "It's blumming " + arr[Math.floor(Math.random()*arr.length)] + "!";
