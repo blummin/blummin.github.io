cd6ef962ac20fb5bfc7606b5e910665a0f5ec073
Callum <58528912+callum11t@users.noreply.github.com>
2024-06-01 23:54:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,5 +54,6 @@ splashText.animate([
 for (let tim = 0; tim < 4; tim++){
   const random = Math.floor(Math.random() * arr.length + 1);
   splashText.innerHTML = arr[Math.floor(Math.random()*arr.length)] + "!";
-    sleep(300);
+  print("Hi")
+  sleep(300);
 }
