80f5e110099768d0af2560bf1df3f75ccb1b89a3
blummin <bluetanimates+2@gmail.com>
2024-06-01 20:54:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const splashText = document.getElementById("splashText");
 const random = Math.floor(Math.random() * 9);
+let words = "having an error"
 
 fetch('/words.txt')
   .then(response => response.text())
   .then((data) => {
     console.log(data)
+    words = data;
   })
