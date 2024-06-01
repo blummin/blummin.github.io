67a2a3b42273162f6316d88d6f930d9f70e9a318
Callum <58528912+callum11t@users.noreply.github.com>
2024-06-01 21:09:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const splashText = document.getElementById("splashText");
 const random = Math.floor(Math.random() * 9);
-let words = null;
-let word = "taking the bloody mick";
 
 fetch('/words.txt')
   .then(response => response.text())
   .then((data) => {
-    console.log(data)
-    words = data;
-    with open('file.txt', 'r') as file:
-      let lines = file.readlines()
-      word = lines[random]
-      print(word)
+    word = data.readline(random);
+    print(word)
   })
